bd43e86d9a1d3a486b7301689e76421e2193a630
rafaelValenca <rafadevcontato@gmail.com>
2023-02-17 10:54:04 -0300

    finish project

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles'); 
+
+
+toggleBtn.addEventListener('click' , () => {
+    document.documentElement.classList.toggle('dark-theme');
+});
+
+
+const articlesData = articles.map((article) => {
+    const {title, date, length, snippet} = article;
+ 
+    const fomatDate = moment(date).format("MMMM Do, YYYY");
+
+    return  `<article class="post">
+                <h2>${title}</h2>
+                <div class="post-info">
+                <span>${fomatDate}</span>
+                <span>${length} min read</span>
+                </div>
+                <p>${snippet}</p>
+            </article>`;
+})
+.join('');
+
+articlesContainer.innerHTML = articlesData;
+
+
